4cf9be1db3dcffe37806dd2196cb6295382b65a9
John Warestone <48253995+Warestone@users.noreply.github.com>
2021-02-14 23:54:45 +0300

    Add swagger2

---
+++ lab2/configuration/Swagger.java
@@ -0,0 +1,36 @@
+package lab2.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder()
+                .title("Spring MVC REST application.")
+                .description("Book shop with postgreSQL.")
+                .version("1.0.0")
+                .contact(new Contact("Evgeny Timofeev", "https://vk.com/warestone","some@email.com"))
+                .license("Apache 2")
+                .build();
+    }
+}
